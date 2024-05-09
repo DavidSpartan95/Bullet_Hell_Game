b41b7a981cb11381e24f578595d000d64282cba2
DavidSpartan95 <davidulwhan@hotmail.com>
2024-05-09 10:49:18 +0200

    Enemy Collision with player resets the game

--- Enemy.gd
+++ Enemy.gd
@@ -7,11 +7,22 @@ extends CharacterBody2D
 
 func _process(delta):
 	if player:
+		for i in range(get_slide_collision_count()):
+			var collision = get_slide_collision(i)
+			if collision.get_collider() == player:
+				print("Collision with player detected")
+				get_tree().reload_current_scene()
+				return
+	
 		# Calculate the direction to the player
 		var direction = (player.global_position - global_position).normalized()
-		velocity = Vector2(direction.x,direction.y) * enemy_move_speed
+		velocity = direction * enemy_move_speed
 		# Move the enemy towards the player
 		move_and_slide()
-
-
+		
+func _on_Enemy_body_entered(body):
+	print("test")
+	if body.name == "player":
+		# Collision with player detected, trigger game reset
+		get_tree().reload_current_scene()  # Reset the game
 
