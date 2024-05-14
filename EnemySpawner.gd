f9f118ebb104d74811dcc347be80e41c0eb9cc14
david <davidulwhan@hotmail.com>
2024-05-14 16:27:30 +0200

    enemy spawns faster the more kills player has

--- EnemySpawner.gd
+++ EnemySpawner.gd
@@ -9,8 +9,17 @@ func _on_timeout():
 	var kinds = enemys[randi()% enemys.size()]
 	var enemy = kinds.instantiate()
 	enemy.player = player
-	enemy.position = Vector2(randf_range(10,990), randf_range(10,590))
+# Ensure the enemy is at least 500 points away on both x and y coordinates
+	var x_offset = randf_range(500, 990)
+	var y_offset = randf_range(500, 590)
+	if randf() < 0.5:
+		x_offset = -x_offset
+	if randf() < 0.5:
+		y_offset = -y_offset
+	
+	enemy.position = Vector2(player.position.x + x_offset, player.position.y + y_offset)
+	enemy.enemy_move_speed = enemy.enemy_move_speed + (5*player.kills)
 	add_child(enemy)
 	
 	
-	wait_time = randf_range(5,7)
+	wait_time = 5/(player.kills+1)
